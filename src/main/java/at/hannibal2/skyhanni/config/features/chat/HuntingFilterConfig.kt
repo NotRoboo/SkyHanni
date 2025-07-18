835a3f52bd40e2a57b5b2e085d2d3d62b33db9ab
NotRoboo <broccoliraids@gmail.com>
2025-07-18 04:16:18 -0400

    Chat Config Overhaul

--- src/main/java/at/hannibal2/skyhanni/config/features/chat/HuntingFilterConfig.kt
+++ src/main/java/at/hannibal2/skyhanni/config/features/chat/HuntingFilterConfig.kt
@@ -8,9 +8,25 @@ import io.github.notenoughupdates.moulconfig.annotations.SearchTag
 class HuntingFilterConfig {
 
     @Expose
+    @ConfigOption(name = "Charm", desc = "Hides charm messages from killing mobs")
+    @SearchTag("salt shard")
+    @ConfigEditorBoolean
+    var charm: Boolean = false
+
+    @Expose
     @ConfigOption(name = "Redundant Comments", desc = "Hide redundant comments from successfully hunting shards.")
-    @SearchTag("panda mochibear invisibug")
+    @SearchTag("panda mochibear invisibug joydive cod")
     @ConfigEditorBoolean
     var redundantComments: Boolean = false
+
+    @Expose
+    @ConfigOption(name = "Salt", desc = "Hides salt messages from killing mobs")
+    @SearchTag("charm shard")
+    var salt: Boolean = false
+
+    @Expose
+    @ConfigOption(name = "Swoop Huntaxe", desc = "Hide Swoop's message about monsters only taking damage from axes.")
+    @ConfigEditorBoolean
+    var swoopAxeMessage: Boolean = false
 
 }
